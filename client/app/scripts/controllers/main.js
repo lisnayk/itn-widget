3507273e69b26d0128bcae7c3bc149ab6e1ca16a
munspel <munspel@Matmod-5>
2016-07-24 12:08:52 +0300

    Second widjet commit

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name angApp.controller:MainCtrl
@@ -10,63 +9,75 @@
 angular.module('angApp')
         .controller('MainCtrl', ['$scope', 'cfpLoadingBar', 'Events', "Masters", "RecTimes", 'JobList', 'Log', 'Auth', "config", function ($scope, cfpLoadingBar, Events, Masters, RecTimes, JobList, Log, Auth, config) {
                 var main = this;
+                //PUBLIC FIELDS
                 main.mastersList = [];
                 main.currentMaster = {};
-                
                 main.checkInDate = 0;
                 main.checkInTime = {};
-                main.activeScreen = 0;
-                main.jobsList = []
+                main.activeScreen = -1;
+                main.jobsList = [];
                 main.clientReguest = {
                     name: "",
                     lastname: "",
                     phone: "",
                     jobs: {}
-
-                }
-
+                };
                 //PUBLIC METHODS
                 main.nextCheckInDate = function () {
                     main.checkInDate++;
-                }
+                };
                 main.prevCheckInDate = function () {
                     main.checkInDate--;
-                }
+                };
                 main.getDate = function (days) {
                     var date = new Date();
                     date = new Date(date.setTime(date.getTime() + days * 86400000));
                     return date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
-                }
+                };
                 main.checkIn = function (item) {
                     //console.log(item);
                     main.checkInTime = item;
                     main.activeScreen = 1;
-                }
+                };
                 main.selectJob = function (job) {
                     var index = job.length - 1;
-                    console.log(job);
+                    //console.log(job);
                     for (var key in main.jobsList.data) {
-                        if (job[0] == main.jobsList.data[key][0]) {
+                        if (job[0] === main.jobsList.data[key][0]) {
                             main.jobsList.data[key][index] = !main.jobsList.data[key][index];
                         }
                     }
-                    console.log(main.jobsList.data);
+                    //console.log(main.jobsList.data);
 
-                }
+                };
                 main.selectedJobs = function () {
                     var index = 3;
                     var list = [];
                     for (var key in main.jobsList.data) {
                         if (main.jobsList.data[key][index]) {
-                            list.push( main.jobsList.data[key]);
+                            list.push(main.jobsList.data[key]);
                         }
                     }
                     return list;
                 };
+                main.reload = function(){
+                    run();
+                }
+                        
                 //PRIVATE METHODS
+                function handleFinally() {
+                    cfpLoadingBar.complete();
+                }
                 function loadRecTimes() {
                     cfpLoadingBar.start();
-                    if (main.currentMaster) {
+                    function handleResolve(data) {
+                        main.recTimes = data;
+                    }
+                    function handleReject() {
+                        console.log("Oops, data could not be loaded.");
+                    }
+                    
+                    if (Object.keys(main.currentMaster).length !== 0) {
                         RecTimes.query({}, {
                             id: main.currentMaster.id,
                             date: main.getDate(main.checkInDate) + "T05:00:00.000Z"
@@ -74,65 +85,63 @@ angular.module('angApp')
                         }).$promise.then(
                                 handleResolve,
                                 handleReject
-                                ).finally(handleFinally)
+                                ).finally(handleFinally);
                     }
-                    function handleResolve(data) {
-                        main.recTimes = data;
-                        //console.log(data);
-                    }
-                    function handleReject() {
-                        alert("Oops, data could not be loaded.");
-                    }
-
                 }
                 function loadJobList() {
                     cfpLoadingBar.start();
-                    if (main.currentMaster) {
-                        JobList.query({}, {
-                            id: main.currentMaster.id,
-                        }).$promise.then(
-                                handleResolve,
-                                handleReject
-                                ).finally(handleFinally)
-                    }
                     function handleResolve(data) {
-                        //console.log(data);
                         for (var key in data.data) {
                             data.data[key].push(false);
                         }
                         main.jobsList = data;
-                        //console.log(data);
                     }
                     function handleReject() {
-                        alert("Oops, data could not be loaded.");
+                        console.log("Oops, data could not be loaded.");
+                    }
+                    if (main.currentMaster) {
+                        JobList.query({}, {
+                            id: main.currentMaster.id
+                        }).$promise.then(
+                                handleResolve,
+                                handleReject
+                                ).finally(handleFinally);
                     }
 
-                }
-                function handleFinally() {
-                    cfpLoadingBar.complete();
-                }
 
-                // APP START
-                cfpLoadingBar.start();
-                Auth.then(function (info) {
-
+                }
+                function reset(){
+                    main.mastersList = [];
+                    main.currentMaster = {};
+                    main.checkInDate = 0;
+                    main.checkInTime = {};
+                    main.activeScreen = -1;
+                    main.jobsList = [];
+                    main.clientReguest = {
+                        name: "",
+                        lastname: "",
+                        phone: "",
+                        jobs: {}
+                    };
+                }
+                function run() {
                     // we can create an instance as well
                     Log.addLog(3);
-
+                    reset();
                     main.mastersList = Masters.query({}, {
                         id: config.clientId
                     }).$promise.then(function (data) {
-
                         main.mastersList = data;
-                        //console.log(data);
                         if (main.mastersList && main.mastersList.data.length > 0) {
                             main.currentMaster = main.mastersList.data[0];
                         }
+                    }).finally(function(){
+                        main.activeScreen = 0;
                     });
-                    //main.data = Events.query();
-                });
-
-
+                }
+                // APP START
+                cfpLoadingBar.start();
+                Auth.then(run);
                 // Watches
                 $scope.$watch('main.currentMaster', function (newValue, oldValue) {
                     if (newValue === oldValue) {
--- views/js/index.php
+++ views/js/index.php
@@ -1,7 +1,51 @@
+
+function animateLeft(obj, from, to, speed){
+   if(from >= to){         
+       //obj.style.visibility = 'hidden';
+        obj.style.right = to + "px";
+       return;  
+   }
+   else {
+   	   obj.style.visibility = 'visible';
+       var box = obj;
+       box.style.right = from + "px";
+       setTimeout(function(){
+           animateLeft(obj, from + speed, to, speed);
+       }, 0) 
+   }
+}
+function animateRight(obj, from, to, speed){
+   if(from <= to){         
+       //obj.style.visibility = 'hidden';
+       obj.style.right = to + "px";
+       return;  
+   }
+   else {
+    	 obj.style.visibility = 'visible';
+       var box = obj;
+       box.style.right = from + "px";
+       setTimeout(function(){
+           animateRight(obj, from - speed, to,speed);
+       }, 0) 
+   }
+}
+
 window.onload = function() {
+    var holder = document.createElement("div");
     var iframe = document.createElement('iframe');
     var btn = document.createElement("div");
     var parent = document.createElement("div");
+    
+    holder.style.width = "500px";
+    holder.style.height = "550px";
+    holder.style.position = "absolute";
+    holder.style.right = "0px";
+    //holder.style.border = "1px solid #ccc";
+    holder.style.margin = "0px";
+    holder.style.bottom = "0px";
+    holder.style.overflow = "hidden";
+    
+    
     
     btn.style.position = "absolute";
     btn.innerHTML ="&#215;"
@@ -9,18 +53,28 @@ window.onload = function() {
     //btn.style.height = "20px";
     btn.style.top = "-25px";
     btn.style.left = "-25px";
+    btn.style.cursor = "pointer";
    // btn.style.border ="1px solid #ccc";
     //btn.style.borderRadius  = "11px";
          btn.style.fontSize = "25px";
          btn.onclick=function(){
-         console.log(iwParent.style.width );
-            if (iwParent.style.width == "0px"){
-                iwParent.style.width ="400px";
-                iwParent.style.border = "1px solid #ccc";
+            var w = iwParent.offsetWidth + parseInt(iwParent.style.marginLeft.replace("px","")) + parseInt(iwParent.style.marginRight.replace("px",""));
+            var r= iwParent.style.right;
+            console.log(w);
+            console.log(r);
+            console.log(iwParent.style.right);
+            if (iwParent.style.right == '0px'){
+                animateRight(iwParent, 0, -w, 5);
+                btn.innerHTML ="&#171;";
+              
             } else {
-                iwParent.style.width ="0px";
-                iwParent.style.border = "0px";
+                
+                animateLeft(iwParent, -w, 0, 5);
+                
+                btn.innerHTML ="&#215;";
+               
             }
+             console.log(iwParent.style.right);
          };
     parent.id = "iwParent";
     parent.style.width = "400px";
@@ -40,5 +94,6 @@ window.onload = function() {
     
     parent.appendChild(btn);
     parent.appendChild(iframe);
-    document.body.appendChild(parent);
+    holder.appendChild(parent);
+    document.body.appendChild(holder);
 };
